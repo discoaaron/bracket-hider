659846f5b2f3baa23b1a650eca601635481847ee
Aaron Small <discoaaron2@gmail.com>
2021-12-08 08:34:45 +1300

    Create hider.js

---
+++ hider.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Liquidpedia bracket result hider
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://liquipedia.net/*
+// @icon         https://www.google.com/s2/favicons?domain=liquipedia.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var bracketScores = document.getElementsByClassName('bracket-score');
+    for (let i = 0; i < bracketScores.length; i += 1) {
+        console.log(bracketScores[i]);
+        bracketScores[i].style.display = "none";
+    }
+})();
