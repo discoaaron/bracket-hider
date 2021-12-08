8aa596472220173816668690a731165582a98872
Aaron Small <discoaaron2@gmail.com>
2021-12-08 19:37:37 +1300

    Update hider.js

--- hider.js
+++ hider.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Liquidpedia bracket result hider
+// @name         Liquidpedia bracket results hider
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -11,13 +11,30 @@
 
 (function() {
     'use strict';
+    // hide bracket score
     var bracketScores = document.getElementsByClassName('bracket-score');
     for (let i = 0; i < bracketScores.length; i += 1) {
-        console.log('aaron was here!');
+        console.log('hiding bracket score');
         bracketScores[i].style.display = "none";
     }
-    
-    
-    document.getElementsByClassName('bracket-popup-body');
 
+    // hide bracket popup body
+    var bracketPopupBody = document.getElementsByClassName('bracket-popup-body');
+    for (let i = 0; i < bracketPopupBody.length; i += 1) {
+        console.log('hiding bracket popup body');
+        bracketPopupBody[i].style.display = "none";
+    }
+
+    // hide teams in the bracket to show when clicked
+    var teamBrackets = document.getElementsByClassName('team-template-team-bracket');
+    for (let i = 0; i < teamBrackets.length; i += 1) {
+        console.log('hiding team bracket');
+        teamBrackets[i].style.display = "none";
+    }
 })();
+
+
+// in case i need to do something like this
+//teamBrackets[i].addEventListener('click', function(event){
+//   event.path[1].style.display = "none";
+// });
